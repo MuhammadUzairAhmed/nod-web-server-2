28ce67c29e23584000101e1cc32ddd93c15af1ae
uzair1494 <uzairahmed1494@gmail.com>
2019-02-12 18:32:34 +0500

    make it better

---
+++ server.js
@@ -0,0 +1,60 @@
+var express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+
+//creating partial view
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.set('view engine','hbs');
+
+
+//hbs helper funciton
+hbs.registerHelper('getCurrentYear',() => {
+return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+return text.toUpperCase();
+});
+
+//it takes middle ware function
+app.use(express.static(__dirname+ '/public'));
+
+/*
+app.get('/', (req,res) => {
+   // res.send('<h1>Hello Express</h1>');
+   res.send({
+name: 'uzair',
+likes: [
+    'abc',
+    'xyz'
+]
+   });
+});
+
+*/
+app.get('/',(req,res)=> {
+    res.render('home.hbs',{
+        pageTitle: "Home",
+        welcomMesg: "Welcome to  y website",
+       
+    });
+});
+
+
+app.get('/about',(req,res)=> {
+    res.render('about.hbs',{
+        pageTitle: "About",
+     
+    });
+});
+
+
+app.get('/bad',(req,res)=>{
+res.send({errMeasge: 'bad request'});
+});
+app.listen(3000, () => {
+    console.log("server is run on port 3000");
+});
